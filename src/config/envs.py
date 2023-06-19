4cd25732f8f7f1801a61d8c5d52dd99be82a8203
Facundo Veronelli <fveronelli@gruposimpli.com>
2023-06-19 13:50:43 -0300

    add new envs

--- src/config/envs.py
+++ src/config/envs.py
@@ -1,9 +1,13 @@
 from pydantic import BaseSettings
 
+class MongoDBSetting(BaseSettings):
+    MONGO_INITDB_ROOT_USERNAME: str
+    MONGO_INITDB_ROOT_PASSWORD: str
+
 class CredentialsSettings(BaseSettings):
     SECRET_KEY: str 
 
-class Settings(CredentialsSettings):
+class Settings(CredentialsSettings, MongoDBSetting):
     ...
 
 settings = Settings()
