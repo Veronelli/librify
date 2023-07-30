573adfe8a29f576896d9c88cb403b65f0a127c2c
root <root@facu-PC.fibertel.com.ar>
2023-07-30 17:12:16 -0300

    change test name

--- tests/unit_tests/test_users.py
+++ tests/unit_tests/test_users.py
@@ -6,7 +6,7 @@ import pytest
 
 # Test create user
 @pytest.mark.anyio
-async def test_create_user_is_success(app_client: TestClient, mock_mongo_motor_client, mocked_users: list[dict[str, Any]]):
+async def test_list_user_is_success(app_client: TestClient, mock_mongo_motor_client, mocked_users: list[dict[str, Any]]):
     response = await app_client.get("/users/list")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == mocked_users
