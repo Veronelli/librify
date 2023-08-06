df8228107643c62ddd024fbb51452a3e1fe997ed
root <root@facu-PC.fibertel.com.ar>
2023-08-05 23:18:32 -0300

    add hash password

--- src/users/services.py
+++ src/users/services.py
@@ -1,10 +1,12 @@
-import bcrypt
+from src.users.models import InputUser
 from src.users.repository import (
     find_all_users,
     find_user_by_id,
     register_user,
     delete_user,
     update_user)
+import bcrypt
+from src.config.envs import settings
 
 async def get_users(
     offset:int|None=None,
@@ -14,7 +16,14 @@ async def get_users(
 async def get_user(id):
     return await find_user_by_id(id)
 
-async def create_user(user):
+async def create_user(user: InputUser):
+    salt = bcrypt.gensalt()
+    user.password = (
+        bcrypt.hashpw(
+        password=user.password.encode('utf-8'),
+        salt=salt)
+        )
+    
     return await register_user(user)
 
 async def update_user_by_id(
